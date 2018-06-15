a4dec214a40d91547a995268ca6364d17c971229
Pavel Mednikov <pavel.mednikov@infoculture.ru>
2018-06-15 13:37:51 +0300

    add transition-group animation

--- scripts.js
+++ scripts.js
@@ -36,6 +36,30 @@ new Vue ({
       this.copiedVisibility = true
       setTimeout(()=>{ this.copiedVisibility = false }, 1500);
       window.getSelection().removeAllRanges()
+    },
+    beforeEnter(el) {
+      el.style.opacity = 0
+      el.style.height = 0
+    },
+    enter(el, done) {
+      var delay = el.dataset.index * 150
+      setTimeout(function () {
+        Velocity(
+          el,
+          { opacity: 1, height: '1.6em' },
+          { complete: done }
+        )
+      }, delay)
+    },
+    leave(el, done) {
+      var delay = el.dataset.index * 150
+      setTimeout(function () {
+        Velocity(
+          el,
+          { opacity: 0, height: 0 },
+          { complete: done }
+        )
+      }, delay)
     }
   },
 
